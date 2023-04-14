00aef54f1fe3bb848b10872490676ab4e8a406f0
Paulina Niedzialek <paulina.niedzialek@gmail.com>
2023-04-14 14:05:06 +0200

    Add ChromeOptions

--- src/test/java/org/nowyprojekt/TestScenario.java
+++ src/test/java/org/nowyprojekt/TestScenario.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestScenario {
 
@@ -11,6 +12,11 @@ public class TestScenario {
     public void someTest() {
         WebDriverManager.chromedriver().setup();
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.komputronik.pl/");
     }
 }
