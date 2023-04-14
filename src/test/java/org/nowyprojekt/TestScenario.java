9032b0cef95db168f539e11d598e36d2dfb887d2
Paulina Niedzialek <paulina.niedzialek@gmail.com>
2023-04-14 13:52:35 +0200

    Add WebDriverManager

--- src/test/java/org/nowyprojekt/TestScenario.java
+++ src/test/java/org/nowyprojekt/TestScenario.java
@@ -1,11 +1,16 @@
 package org.nowyprojekt;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestScenario {
 
     @Test
     public void someTest() {
+        WebDriverManager.chromedriver().setup();
 
+        WebDriver driver = new ChromeDriver();
     }
 }
